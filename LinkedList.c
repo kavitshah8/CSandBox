b2b9d08ec457b56d1bd8087d651b63f65a475376
Kavit Shah <kavit.shah8@gmail.com>
2013-10-14 12:13:23 -0400

    Document the code

--- LinkedList.c
+++ LinkedList.c
@@ -1,13 +1,19 @@
+/*
+Concepts: Data structure starting with single linked list
+*/
 #include<stdio.h>
 #include<stdlib.h>
 
+// defining struct linkedlist as ll
 typedef struct linkedlist ll;
 
+// signly linked list with a pointer and a value 
 struct linkedlist{
 	ll *ptr;
 	int value;
 };
 
+// declaring functions to manipulate singly linked list 
 ll* insert_in_front(ll *,int);
 ll* delete_in_front(ll *);
 int traverse(ll *head);
@@ -15,25 +21,36 @@ int traverse(ll *head);
 int main(){
 
 	int i;
+	// creating a first header pointer (pivot for linked list)
 	ll *head = NULL;
 
+	// insterting elements in the linked list
 	for(i=0;i<=9;i++){
 		head = insert_in_front(head,i);
 	}
 
+	// checking elements are inserted properly or not ?
 	traverse(head);
 	printf("\n");
 
+	// delelting elements from the linked list
 	for(i=0;i<=1;i++){
 		head = delete_in_front(head);
 	}
 	
+	// checking elements are deleted properly or not ?
 	traverse(head);
 	printf("\n");
 
 	return 0;
 }
 
+/* Logic for inserting a new node in the front of linked list 
+Step 1: Create a pointer to new block of memory, assign a value using pointer variable
+Step 2:	Check base case
+Step 3: Link new node with last node in linked list using header
+Step 4: Redirect header
+*/
 ll* insert_in_front(ll *head,int i){
 	ll *n;
 	n = malloc(sizeof(ll));
@@ -48,6 +65,13 @@ ll* insert_in_front(ll *head,int i){
 	return head;
 }
 
+/* Logic for deleting a node in the front of linked list 
+Step 1: Create a temporary pointer
+Step 2:	Is there any element to delete from Linked List ?
+Step 3: Redirect new pointer to second last node in linked list using header
+Step 4: Free last node
+Step 5: Redirect header
+*/
 ll* delete_in_front(ll *head){
 	ll *temp;
 	if(head==NULL){
@@ -60,6 +84,11 @@ ll* delete_in_front(ll *head){
 	return head;
 }
 
+/* Logic for traversing a linked list from the front 
+Step 1:	Traverse till the header points to NULL
+Step 2: Print value of current node
+Step 5: Redirect header to next node in the list
+*/
 int traverse(ll *head){
 
 	while(head != NULL){
