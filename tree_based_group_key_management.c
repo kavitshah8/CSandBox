c1bd55795027f8742344341e0417ff34f6fe1f04
Kavit Shah <kavit.shah8@gmail.com>
2013-12-18 19:13:59 -0500

    Join Protocol(successful compilation)

--- tree_based_group_key_management.c
+++ tree_based_group_key_management.c
@@ -6,169 +6,139 @@ Thesis: Tree based Group Key Management
 #include<stdlib.h>
 #include<math.h>
 
-/*
-	NODES represents # of intermediate nodes (ie without counting the leaf nodes)
-	HEIGHT represents the height of a tree
-	Binary Tree < = > Number of intermediate nodes = (2^ height of a tree - 1)
-	These two globale variables helps identify the binary tree using above formula
-*/
-extern int NODES;
-extern int HEIGHT;
+//Global variables
+int NODES;
+int HEIGHT;
 
 //Data structure
-typedef struct
+struct node_t
 {
-	// unit32_t (needs to figure out)
-	unsigned int publicValue, privateValue, height, depth, type;
-	struct node_t *left;
-	struct node_t *right;
-	struct node_t *next;
-	struct node_t *parent;
-}node_t;
+        unsigned int publicValue, privateValue, height, depth, type;
+        struct node_t *left;
+        struct node_t *right;
+        struct node_t *next;
+        struct node_t *parent;
+};
+
 
 // Function Prototypes
-node_t * new_node(unsigned int);
-node_t * find_sponsor(node_t **);
-void join(node_t **, node_t **);
-void update_tree(node_t **, node_t **);//, node_t **, node_t **);
-void update_depth(node_t *);
+void join(struct node_t **,struct node_t **);
+struct node_t * find_sponsor(struct node_t **);
+struct node_t * new_node(unsigned int);
+//void update_tree(struct node_t **, struct node_t **);//, struct node_t **, struct node_t **);
+//void update_depth(struct node_t *);
 
 int main()
 {
-	node_t *root=NULL;
-	node_t *header=NULL;
-	NODES = 0;
-	HEIGHT = 0;
-	join(&root , &header);
-	
-	return 0;
-}
-// Checked
-node_t * new_node(unsigned int i)
-{	
-	node_t *temp;
-	temp = (node_t*) malloc(sizeof(node_t));
-	return temp;
-}
-node_t * find_sponsor(node_t ** header)
-{
-	int d;
-	node_t * sponsor;
-	node_t * temp;
-	*temp = (*header);
-	
-	d =0;
+        struct node_t *root=NULL;
+        struct node_t *header=NULL;
+        NODES = 0;
+		HEIGHT =0;
+        join(&root , &header);
 
-	while(*temp != NULL)
-	{
-		if((*temp)->depth) <= d)
-		{
-			d = (*temp)->depth;
-			*sponsor = temp;
+        return 0;
+}
+struct node_t * new_node(unsigned int i)
+{
+	struct node_t * ret;
+	ret = (struct node_t*)malloc(sizeof(struct node_t));
+	return ret;
+}
+
+void join(struct node_t ** root, struct node_t ** header)
+{
+        struct node_t *leaf;
+        struct node_t *node;
+        if(*root == NULL)
+        {        
+                 // 1- leaf ; 0 - node
+                 leaf = new_node(1);
+                 (*root) = leaf;
+                 (*header) = leaf;
+        }
+        else
+        {
+			struct node_t *sponsor;
+			struct node_t * leaf,* temp,*node;
+			leaf = new_node(1);
+			node = new_node(0);
+			NODES ++;
+			// BASE CASE
+			if (NODES == 1)
+			{
+                (node)->left = (*root);
+                (node)->right = (leaf);
+                (node)->height = 1;
+
+                (*root)->parent = (node);
+                (*root)->next = (leaf);
+                (*root)->depth = (node)->height + 1;
+
+                (leaf)->parent = (node);
+                (leaf)->next = NULL;
+                (leaf)->depth = (node)->height + 1;
+
+                (*root) = (node);
+                HEIGHT++;
+			}        
+			else if(NODES == ((2^HEIGHT) - 1))
+			{
+                (node)->left = (*root);
+                (node)->right = (leaf);
+                (node)->height = (*root)->height + 1;
+
+                (*root)->parent = (node);
+                (*root) = (node);
+                //depth_update(header);
+                //(*root)->next = (*leaf);
+                //(*root)->depth = (*node)->height + 1;
+
+                (leaf)->parent = (node);
+                (leaf)->next = NULL;
+                (leaf)->depth = 1;
+
+                sponsor = find_sponsor(header);
+                (*root) = (node);
+
+                HEIGHT++;
+			}
+			else
+			{
+				sponsor = find_sponsor(header);
+				(node)->left = sponsor;
+                (node)->right = (leaf);
+				(node)->parent = sponsor->parent;
+				(node)->height = sponsor->depth;
+				
+				sponsor->parent = node;
+				sponsor->next = leaf;
+				sponsor->depth ++;
+
+				leaf->next = NULL;
+				leaf->parent = (node);
+				leaf->depth = sponsor->depth;
+			}
+
 		}
-		temp = temp->next;
-	}
-	return sponsor;
 }
-// Checked
-void join(node_t ** root, node_t ** header)
+
+struct node_t * find_sponsor(struct node_t ** header)
 {
-	node_t *leaf;
-	node_t *node;
-	
-	if(*root == NULL)
-	{	
-		 // 1- leaf ; 0 - node
-		 leaf = new_node(1);
-		 (*root) = leaf;
-		 (*header) = leaf;
-	}
-	else
-	{
-		/*
-		leaf = new_node(1);
-		node = new_node(0);
-		NODES ++;
-		// */
-		update_tree(root, header);//, &node, &leaf);
-	}
+        int d;
+        struct node_t * sponsor;
+        struct node_t * temp;
+        temp = (*header);
+        
+        d =0;
+
+        while(temp !=NULL)
+        {
+                if((temp)->depth <= d)
+                {
+                        d = (temp)->depth;
+                        sponsor = temp;
+                }
+                temp = temp->next;
+        }
+        return sponsor;
 }
-void update_tree(node_t **root, node_t **header)//, node_t **node, node_t **leaf)
-{
-	node_t *sponsor;
-	node_t *temp;
-	node_t *leaf;
-	node_t *node;
-	leaf = new_node(1);
-	node = new_node(0);
-	NODES ++;
-	// BASE CASE
-	if (NODES == 1)
-	{
-		(node)->left = &(*root); 
-		(node)->right = (*leaf);
-		(node)->height = 1;
-
-		(*root)->parent = (*node);
-		(*root)->next = (*leaf);
-		(*root)->depth = (*node)->height + 1;
-
-		(*leaf)->parent = (*node);
-		(*leaf)->next = NULL;
-		(*leaf)->depth = (*node)->height + 1;
-
-		(*root) = (*node); 
-		
-		HEIGHT++;
-	}
-	else if(NODES == ((int)pow(2,(float)HEIGHT) - 1))
-	{
-		(*node)->left = (*root); 
-		(*node)->right = (*leaf);
-		(*node)->height = (*root)->height + 1;
-
-		(*root)->parent = (*node);
-		(*root) = (*node);
-		// depth_update(header);
-		//(*root)->next = (*leaf);
-		//(*root)->depth = (*node)->height + 1;
-
-		(*leaf)->parent = (*node);
-		(*leaf)->next = NULL;
-		(*leaf)->depth = 1;
-
-		sponsor = find_sponsor(header);
-		(*sponsor)->next = (*leaf);
-
-		
-		NODES ++;
-		HEIGHT++;
-	}
-
-	else
-	{
-		sponsor = find_sponsor(header);
-		(*node)->left = sponsor; 
-		(*node)->right = (*leaf);
-		(*node)->parent = sponsor->parent;
-		(*node)->height = (*root)->height + 1;
-		
-		temp = (*sponsor)->next; 
- 		(*sponsor)->next = (*leaf);
-		(*sponsor)->parent =(*node);
-		sponsor->depth ++;
-
-		(*leaf)->parent = (*node);
-		(*leaf)->next = temp;
-		(*leaf)->depth = sponsor->depth;
-		
-		NODES ++;
-	}
-
-}
-void update_depth(node_t *)
-{
-
-}
-
-
