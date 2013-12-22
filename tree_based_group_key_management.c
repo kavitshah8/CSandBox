8cffd3058d702035fe2e05bb9520ba59de558742
Kavit Shah <kavit.shah8@gmail.com>
2013-12-22 15:52:53 -0500

    Join Protocol successful comilation with two functions

--- tree_based_group_key_management.c
+++ tree_based_group_key_management.c
@@ -13,7 +13,7 @@ int HEIGHT;
 //Data structure
 struct node_t
 {
-        unsigned int publicValue, privateValue, height, depth, type;
+        unsigned int publicValue, privateValue, type,depth;
         struct node_t *left;
         struct node_t *right;
         struct node_t *next;
@@ -23,9 +23,9 @@ struct node_t
 
 // Function Prototypes
 void join(struct node_t **,struct node_t **);
-struct node_t * find_sponsor(struct node_t **);
+//struct node_t * find_sponsor(struct node_t **);
 struct node_t * new_node(unsigned int);
-//void update_tree(struct node_t **, struct node_t **);//, struct node_t **, struct node_t **);
+int size(struct node_t *);
 //void update_depth(struct node_t *);
 
 int main()
@@ -35,20 +35,29 @@ int main()
         NODES = 0;
 		HEIGHT =0;
         join(&root , &header);
-
+		//printf("%d\n",size(root));
         return 0;
 }
 struct node_t * new_node(unsigned int i)
 {
-	struct node_t * ret;
-	ret = (struct node_t*)malloc(sizeof(struct node_t));
-	return ret;
+	struct node_t * node;
+	
+	node = (struct node_t*)malloc(sizeof(struct node_t));
+	
+	node->left = NULL;
+	node->right = NULL;
+	node->parent = NULL;
+	node->next = NULL;
+	node->depth = 0;
+	return node;
 }
 
 void join(struct node_t ** root, struct node_t ** header)
 {
-        struct node_t *leaf;
-        struct node_t *node;
+        struct node_t *leaf,*node,*temp,*sponsor;
+		int d;
+		d = 0;
+
         if(*root == NULL)
         {        
                  // 1- leaf ; 0 - node
@@ -58,38 +67,48 @@ void join(struct node_t ** root, struct node_t ** header)
         }
         else
         {
-			struct node_t *sponsor;
-			struct node_t * leaf,* temp,*node;
+			
 			leaf = new_node(1);
 			node = new_node(0);
 			NODES ++;
+			
 			// BASE CASE
 			if (NODES == 1)
 			{
                 (node)->left = (*root);
                 (node)->right = (leaf);
-                (node)->height = 1;
+                //(node)->height = 1;
 
                 (*root)->parent = (node);
                 (*root)->next = (leaf);
-                (*root)->depth = (node)->height + 1;
-
+				(*root)->depth = 1;
+                
                 (leaf)->parent = (node);
                 (leaf)->next = NULL;
-                (leaf)->depth = (node)->height + 1;
+				(leaf)->depth = 1;
 
                 (*root) = (node);
+				// same as root's height
                 HEIGHT++;
 			}        
+			
 			else if(NODES == ((2^HEIGHT) - 1))
 			{
-                (node)->left = (*root);
+                //Update Depth of each node
+				temp = *header;
+				while(temp != NULL)
+				{
+					temp -> depth++ ;
+					temp = temp -> next;
+				}
+
+				(node)->left = (*root);
                 (node)->right = (leaf);
-                (node)->height = (*root)->height + 1;
+                //(node)->height = (*root)->height + 1;
 
                 (*root)->parent = (node);
                 (*root) = (node);
-                //depth_update(header);
+                
                 //(*root)->next = (*leaf);
                 //(*root)->depth = (*node)->height + 1;
 
@@ -97,43 +116,68 @@ void join(struct node_t ** root, struct node_t ** header)
                 (leaf)->next = NULL;
                 (leaf)->depth = 1;
 
-                sponsor = find_sponsor(header);
-                (*root) = (node);
+				//sponsor = find_sponsor(header);
+				//Find the sponsor in Balanced Binary case
+				sponsor = *header;
+			    while(sponsor != NULL)
+				{
+					sponsor = sponsor->next;
+				}
+				
+				sponsor->next = leaf;
+
+				(*root) = (node);
 
                 HEIGHT++;
 			}
+			
 			else
 			{
-				sponsor = find_sponsor(header);
+				//sponsor = find_sponsor(header);
+				// To find shallowest right most leaf in tree
+				sponsor = (*header);
+				d = sponsor -> depth;
+
+		        while(sponsor !=NULL)
+				{		
+					if((sponsor)->depth < d)
+					{
+                        d = (sponsor)->depth;
+                        sponsor = temp;
+					}
+					sponsor = sponsor->next;
+				}
+			
 				(node)->left = sponsor;
-                (node)->right = (leaf);
+				(node)->right = (leaf);
 				(node)->parent = sponsor->parent;
-				(node)->height = sponsor->depth;
+				//(node)->height = 1;
 				
 				sponsor->parent = node;
 				sponsor->next = leaf;
-				sponsor->depth ++;
+				//sponsor->depth ++;
 
 				leaf->next = NULL;
 				leaf->parent = (node);
-				leaf->depth = sponsor->depth;
+				//leaf->depth = sponsor->depth;
 			}
 
 		}
 }
 
+// Finds shallowest rightmost leaf
+/*
 struct node_t * find_sponsor(struct node_t ** header)
 {
         int d;
-        struct node_t * sponsor;
-        struct node_t * temp;
-        temp = (*header);
+        struct node_t * sponsor,* temp;
         
-        d =0;
+		temp = (*header);
+		d = temp->depth;
 
         while(temp !=NULL)
-        {
-                if((temp)->depth <= d)
+        {		
+                if((temp)->depth < d)
                 {
                         d = (temp)->depth;
                         sponsor = temp;
@@ -142,3 +186,12 @@ struct node_t * find_sponsor(struct node_t ** header)
         }
         return sponsor;
 }
+*/
+/*
+int size(struct node_t *root){
+        if (root==NULL)
+                return 0;
+        else
+                return(size(root->left)+1+size(root->right));
+}
+*/
